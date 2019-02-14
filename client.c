f299e125c8a147ed3d1a840bad25a3687add5d3c
Ashish Adhikari <ashish.adhikari@bison.howard.edu>
2019-02-13 21:56:02 -0500

    started working on server side

--- client.c
+++ client.c
@@ -17,34 +17,53 @@ void error(const char* msg){
 	exit(EXIT_FAILURE);
 }
 
+
+
 int main(int argc, char **argv){
-	int clPort;
+	int svPort;
 	int clSocket;
 	struct sockaddr_in servAddr;
 	char buffer[BUFFER_SIZE];
     
-
-
-
 	char user_input = 't';
+	//todo implement a proper cl arg parser
+		/* * get and load the proper port number
+		   * 
+		*/
 	if (argc!=3)
 	{
-		error("Usage: %s hostname port\n");
-	
+		error("Usage: %s hostname port\n");	
 	}
 
+
 	//obtain socket descriptor 
+	if ( (clSocket = socket(AF_INET, SOCK_STREAM, 0)) < 0 ) {
+		error("ECHOCLNT: Error creating listening socket.\n");
+    }
+
+    memset(&servAddr, 0, sizeof(servAddr));
+    servAddr.sin_family = AF_INET
+    servAddr.sinport = htons(svPort);
 
 
 	//loop while input != 'q'
 	while (user_input!='q'){
 		switch(user_input){
 			case 's':
+				//TODO implement string handler
+				capString();				
+				break;
+		
+			case 'f':
+				//TODO implement file handler
+				getFile();
+				break;
+		
+			default:
 				break;
 		}
-		
+		printf("Enter 's' (capitalize string), 'f' (receive file), or 'q' (to quit)... (\n" );
 		user_input = getchar();
-
 	}
 
 	return 0;
--- server.c
+++ server.c
@@ -1,6 +1,7 @@
-#include <stdio.h>
-#include <unistd.h> 
-#include <stdlib.h>
+#include <sys/socket.h>       /*  socket definitions        */
+#include <sys/types.h>        /*  socket types              */
+#include <arpa/inet.h>        /*  inet (3) funtions         */
+#include <unistd.h>           /*  misc. UNIX functions      */
 
 int main(){
 
