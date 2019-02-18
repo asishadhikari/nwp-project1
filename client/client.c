dbe3aa3eb06fc96997223d9ee501a170e218506b
Ashish Adhikari <ashish.adhikari@bison.howard.edu>
2019-02-18 14:14:33 -0500

    implemented ip address checks

--- client/client.c
+++ client/client.c
@@ -1,13 +1,15 @@
 /*
   client.c
   ==========
+
   Ashish Adhikari, 2019
 */
 #include <stdio.h>
-
+#include <string.h>
 #include <sys/socket.h>
 #include <stdlib.h>
 #include <arpa/inet.h>
+#include <errno.h>
 #define BUFFER_SIZE 1000 		
 
 
@@ -18,44 +20,45 @@ void error(const char* msg){
 
 
 
-int main(int argc, char **argv){
+int main(int argc, char *argv[]){
+	char *svIP;
 	int svPort;
 	int clSocket;
 	struct sockaddr_in servAddr;
+
 	char buffer[BUFFER_SIZE];
     
 	char user_input = 't';
-	//todo implement a proper cl arg parser
-		/* * get and load the proper port number
-		   * 
-		*/
-	if (argc!=3)
-	{
+	if (argc!=3){
+		errno = EINVAL;
 		error("Usage: %s hostname port\n");	
 	}
 
+	svIP = argv[1];
 
 	//obtain socket descriptor 
-	if ( (clSocket = socket(AF_INET, SOCK_STREAM, 0)) < 0 ) {
+	if ( (clSocket = socket(AF_INET, SOCK_STREAM, 0)) < 0 ) 
 		error("ECHOCLNT: Error creating listening socket.\n");
-    }
-
+    
     memset(&servAddr, 0, sizeof(servAddr));
-    servAddr.sin_family = AF_INET
-    servAddr.sinport = htons(svPort);
-
+    servAddr.sin_family = AF_INET;
+    servAddr.sin_port = htons(svPort);
+    if( !(inet_aton(svIP, &servAddr.sin_addr)) ){
+    	errno = EINVAL;
+    	error("Invalid Ipv4 address provided.");
+    }
 
 	//loop while input != 'q'
 	while (user_input!='q'){
 		switch(user_input){
 			case 's':
 				//TODO implement string handler
-				capString();				
+//				capString();				
 				break;
 		
 			case 'f':
 				//TODO implement file handler
-				getFile();
+//				getFile();
 				break;
 		
 			default:
